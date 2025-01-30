dddab244570eb59ed241788a12b8ae72bb5d940b
Maciej Mądry <maciejmadry0@gmail.com>
2025-01-30 10:38:26 +0100

    FIX: changes products list to suspended component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,10 @@
 import MaxWidthContainer from "../components/MaxWidthContainer";
-import { Badge } from "../components/ui/Badge";
-import { Card } from "../components/ui/Card";
-import { getProducts } from "../lib/actions/product.actions";
+import ProductsList from "../components/shared/productsList/ProductsList";
 
-export default async function Home() {
-  const products = await getProducts();
-  console.log(products);
+export default function Home() {
   return (
     <MaxWidthContainer className="py-8">
-      <div className="grid grid-cols-3 gap-4">
-        {products.map((product) => (
-          <Card key={product._id}>
-            <Card.Header className="border-b">{product.name}</Card.Header>
-            <Card.Content>{product.description}</Card.Content>
-            <Card.Footer className="text-end pt-0">
-              <Badge>${product.price}</Badge>
-            </Card.Footer>
-          </Card>
-        ))}
-      </div>
+      <ProductsList />
     </MaxWidthContainer>
   );
 }
---
+++ app/components/shared/productsList/ProductsList.tsx
@@ -0,0 +1,51 @@
+import { Suspense } from "react";
+
+import { Card } from "../../ui/Card";
+import { Badge } from "../../ui/Badge";
+
+import { getProducts } from "@/app/lib/actions/product.actions";
+import Link from "next/link";
+
+async function ProductList() {
+  const products = await getProducts();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {products.map((product) => (
+        <Link href={`/products/${product.slug}`} key={product._id}>
+          <Card>
+            <Card.Header className="border-b">{product.name}</Card.Header>
+            <Card.Content className="line-clamp-2 w-full pb-0">
+              {product.description}
+            </Card.Content>
+            <Card.Footer className="text-end pt-4">
+              <Badge>${product.price}</Badge>
+            </Card.Footer>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default function ProductListWrapper() {
+  return (
+    <Suspense fallback={<LoadingSkeleton />}>
+      <ProductList />
+    </Suspense>
+  );
+}
+
+function LoadingSkeleton() {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {Array.from({ length: 12 }).map((_, i) => (
+        <div className="border rounded-lg p-4 shadow-md animate-pulse" key={i}>
+          <div className="h-6 bg-gray-300 rounded w-3/4 mb-2"></div>
+          <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
+          <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+        </div>
+      ))}
+    </div>
+  );
+}
